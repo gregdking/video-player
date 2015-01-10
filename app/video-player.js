ef982ecd4ed52ee5138ca88529b8b91159e701c8
Greg King <gregdking@gmail.com>
2015-01-09 20:32:14 -0500

    initial mithril version

--- app/video-player.js
+++ app/video-player.js
@@ -2,13 +2,13 @@ var ViewModel = (function () {
     "use strict";
 
     function ViewModel (){
-        this.applicationTitle = ko.observable('Video Player');
-        this.videos = ko.observableArray([
+        this.applicationTitle = m.prop('Video Player');
+        this.videos = m.prop([
             { name: 'Incredibles', src: 'movies/Incredibles (2004).mp4' },
             { name: 'Ratatouille', src: 'movies/Ratatouille (2007).mp4' },
             { name: 'Toy Story', src: 'movies/Toy Story (1995).mp4' }
         ]);
-        this.selectedVideo = ko.observable(this.videos()[0]);
+        this.selectedVideo = m.prop(this.videos()[0]);
     }
 
     ViewModel.prototype.isSelected = function isSelected(video) {
@@ -22,7 +22,36 @@ var ViewModel = (function () {
     return ViewModel;
 }());
 
-$(function () {
-    var vm = new ViewModel();
-    ko.applyBindings(vm);
+function Controller() {
+    this.vm = new ViewModel();
+}
+
+function view(controller) {
+    var vm = controller.vm;
+    return m('.panel.panel-default', [
+        m('.panel-heading', vm.applicationTitle()),
+        m('.panel-body.container', [
+            m('.row', [
+                m('.video-list.col-lg-4', [
+                    m('.list-group', [
+                        vm.videos().map(function (video) {
+                            return m('a.list-group-item', {
+                                href: '#',
+                                class: vm.isSelected(video) ? 'active' : '',
+                                onclick: vm.selectedVideo.bind(vm, video)
+                            }, video.name)
+                        })
+                    ])
+                ]),
+                m('.video-player.col-lg-8.well.text-center', [
+                    m('video[width=648][height=480][controls]', { src: vm.selectedVideo().src })
+                ])
+            ])
+        ])
+    ]);
+}
+
+m.module(document.querySelector('.root'), {
+    controller: Controller,
+    view: view
 });
